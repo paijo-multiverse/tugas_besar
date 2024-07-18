8a4702c439ee1a6f9b401dec5ada8624413c2ce7
squadpaijoganteng <squadpaijoganteng@gmail.com>
2024-07-18 23:25:18 +0000

    update dari idx

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:tugas_besar/ui/bottom_nav_controller.dart';
 import 'package:tugas_besar/ui/splash_screen.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'firebase_options.dart';
--- lib/ui/registration_screen.dart
+++ lib/ui/registration_screen.dart
@@ -1,7 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:tugas_besar/const/appcolors.dart';
 import 'package:tugas_besar/ui/login_screen.dart';
 import 'package:tugas_besar/ui/user_form.dart';
 
